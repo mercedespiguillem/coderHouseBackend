54a07718f758dad0468f9c873a887ed5191769d7
Maria Mercedes Piguillem <mercedes.piguillem@gmail.com>
2023-07-28 22:28:14 +0200

    primer commit del proyecto

---
+++ desafio_1/productManager.js
@@ -0,0 +1,51 @@
+class Product {
+
+    static #count = 1;
+
+    constructor (title, description, price, thumbnail, code, stock)    {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code  = Product.#count++;
+        this.stock = stock;
+}}
+
+
+class ProductManager {
+
+    constructor() {
+        this.products = [];
+    }
+
+    //metodo addProduct - agrega un producto al array
+
+    addProduct(product) {
+        if (!this.products.filter(product.code)) {
+            this.products.push(product)
+        }
+    }
+    
+    // metodo getProducts 
+    getProducts () {
+        return this.products;
+    }
+
+    // metodo getProductbyId
+    getProductById (id) {
+        const product = this.products.filter(product => product.code == id)
+        if (product != '') {
+            return product;
+        } else {
+            throw new Error("Not found")
+        }     
+            
+        }
+    }
+
+const prod1 =  new Product("buzo", "Buzo de algodon", "2000", "thumbnail.jpg", "50");
+
+console.log(prod1)
+
+
+
