427ce04b391d66d94e61d45159eaf3bc2c59fde9
Maria Mercedes Piguillem <mercedes.piguillem@gmail.com>
2023-07-28 23:55:12 +0200

    Primer commit

--- desafio_1/product.js
+++ desafio_1/product.js
@@ -1,12 +1,14 @@
 class Product {
+  static #count = 1;
 
-    static #count = 1;
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = Product.#count++;
+    this.stock = stock;
+  }
+}
 
-    constructor (title, description, price, thumbnail, code, stock)    {
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code  = Product.#count++;
-        this.stock = stock;
-}}
+console.log("hola");
